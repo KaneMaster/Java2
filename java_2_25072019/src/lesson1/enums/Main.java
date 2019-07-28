1f414b5250361ea3ce9ac0ed36fb50887014e643
KaneMaster <avmahhiboroda@gmail.com>
2019-07-28 14:57:20 +0300

    Добавлена коллекция с днями недели

--- java_2_25072019/src/lesson1/enums/Main.java
+++ java_2_25072019/src/lesson1/enums/Main.java
@@ -2,11 +2,14 @@ package lesson1.enums;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Fruit.Chery);
-        System.out.println(Fruit.Chery == Fruit.Chery);
+     /*   System.out.println(Fruit.Chery);
+       System.out.println(Fruit.Chery == Fruit.Chery);
 
-        for (Fruit f:Fruit.values()) {
-            System.out.println(f+" "+ f.getWeigth());
+        for (Fruit f : Fruit.values()) {
+            System.out.println(f + " " + f.getWeigth());
         }
+    */
+        DayOfWeeks.getWorkingHours(DayOfWeeks.Monday);
+        DayOfWeeks.getWorkingHours(DayOfWeeks.Sunday);
     }
 }
